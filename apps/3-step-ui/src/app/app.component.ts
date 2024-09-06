b7c96af0322b3de1c33fff9d6ccfc5791f87c596
marius <contact@mariusperle.de>
2024-09-06 22:38:08 +0200

    nav

--- apps/3-step-ui/src/app/app.component.ts
+++ apps/3-step-ui/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { WelcomeComponent } from './welcome.component';
 
 @Component({
     standalone: true,
-    imports: [WelcomeComponent, RouterModule],
-    selector: 'app-root',
+    imports: [RouterModule],
+    selector: 'step-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
