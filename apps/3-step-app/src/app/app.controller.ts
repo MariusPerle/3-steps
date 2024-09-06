940bf3dfda265b2989ecea764c75e771c48f31b1
MOXHAN <maxim.hansen2000@gmail.com>
2024-09-06 22:35:16 +0200

    Add Endpoints for the 3 categories

--- apps/3-step-app/src/app/app.controller.ts
+++ apps/3-step-app/src/app/app.controller.ts
@@ -2,12 +2,34 @@ import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
-@Controller()
-export class AppController {
+@Controller("Soon")
+export class SoonExpireController {
     constructor(private readonly appService: AppService) {}
 
     @Get()
-    getData() {
+    getSoonExpireData() {
+
+        // Return JSON here
+        return this.appService.getData();
+    }
+}
+
+@Controller("Now")
+export class ExpiredController {
+    constructor(private readonly appService: AppService) {}
+
+    @Get()
+    getExpiredData() {
+        return this.appService.getData();
+    }
+}
+
+@Controller("Waste")
+export class WasteController {
+    constructor(private readonly appService: AppService) {}
+
+    @Get()
+    getWasteData() {
         return this.appService.getData();
     }
 }
--- apps/3-step-app/src/app/app.service.ts
+++ apps/3-step-app/src/app/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
+// API-Request to Schwarz-IT here
 export class AppService {
     getData(): { message: string } {
         return { message: 'Hello API' };
