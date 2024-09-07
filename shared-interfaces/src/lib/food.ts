c2cfd5d642de507bdb26b1b8ea5ef7085d698ab2
MOXHAN <maxim.hansen2000@gmail.com>
2024-09-07 11:01:08 +0200

    implemented controllers

--- shared-interfaces/src/lib/food.ts
+++ shared-interfaces/src/lib/food.ts
@@ -5,8 +5,24 @@ export class Food {
     price: number;
     weight: string;
     available: number;
+
+    constructor(id: string, name: string, expiresAt: string, price: number, weight: string, available: number) {
+        this.id = id;
+        this.name = name;
+        this.expiresAt = new Date(expiresAt);
+        this.price = price;
+        this.weight = weight;
+        this.available = available;
+    }
 }
 
 export class FoodExpiresSoon extends Food {
     discountInPercent: number;
+
+    constructor(id: string, name: string, expiresAt: string, price: number, weight: string, available: number, discountInPercent: number) {
+        super(id, name, expiresAt, price, weight, available);
+        this.discountInPercent = discountInPercent;
+    }
+
+    
 }
