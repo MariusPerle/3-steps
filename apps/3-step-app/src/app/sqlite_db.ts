ba6c2aa13ff1a55bba21d2d2d327043fda1cdc39
MOXHAN <maxim.hansen2000@gmail.com>
2024-09-07 17:17:01 +0200

    implement class for sqlite db

---
+++ apps/3-step-app/src/app/sqlite_db.ts
@@ -0,0 +1,35 @@
+import { Database } from 'sqlite3';
+
+export class foodDatabase {
+
+    private db: Database;
+
+    constructor() {
+        // open db stored in file "inventory.db"
+        this.db = new Database('inventory.db')
+
+        // Drop table for testing only
+        this.db.exec("DROP TABLE food")
+        
+        // if table food does not exist, create it
+        this.db.exec("CREATE TABLE IF NOT EXISTS food (id INT PRIMARY KEY, name VARCHAR(255), expiresAt VARCHAR(120), price REAL, weight VARCHAR(120), packagingUnit VARCHAR(120), available INTEGER)");
+
+    }
+
+    getConnection() {
+        return this.db;
+    }
+
+    writeToDb(cmd: string) {
+        this.db.exec(cmd);
+    }
+
+    insertIntoDb(id: number, name: string, expiresAt: string, price: number, weight: string, packagingUnit: string, available: number) {
+        this.db.exec(`INSERT INTO food VALUES (${id}, '${name}', '${expiresAt}', ${price}, '${weight}', '${packagingUnit}', ${available})`);
+    }
+
+}
+
+
+
+
