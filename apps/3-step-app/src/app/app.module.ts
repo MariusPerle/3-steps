831510f27bd3c20d073c5881f313cf799131c005
Tobias Bungard <tobi-bungard@t-online.de>
2024-09-07 09:36:54 +0200

    Using downloaded articles.json

--- apps/3-step-app/src/app/app.module.ts
+++ apps/3-step-app/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
+import { SoonExpireController, ExpiredController, WasteController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
     imports: [],
-    controllers: [AppController],
+    controllers: [SoonExpireController, ExpiredController, WasteController],
     providers: [AppService],
 })
 export class AppModule {}
--- apps/3-step-app/src/app/app.service.ts
+++ apps/3-step-app/src/app/app.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import { ClaimItemDto } from './claim-item.dto';
 
 @Injectable()
 // API-Request to Schwarz-IT here
 export class AppService {
-    getData(): { message: string } {
-        return { message: 'Hello API' };
+
+  private data: object;
+
+  constructor() {
+    this.data = JSON.parse(
+      fs.readFileSync('articles.json', 'utf-8')
+    );
+  }
+
+    getData(): object {
+        return this.data;
     }
+
+  claimItem(dto: ClaimItemDto) {
+    console.log(dto);
+  }
 }
