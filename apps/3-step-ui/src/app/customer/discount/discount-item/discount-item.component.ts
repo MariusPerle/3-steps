637c891b8c2a84e1491288f8765e020f11012b74
marius <contact@mariusperle.de>
2024-09-07 11:14:58 +0200

    display discounted price

--- apps/3-step-ui/src/app/customer/discount/discount-item/discount-item.component.ts
+++ apps/3-step-ui/src/app/customer/discount/discount-item/discount-item.component.ts
@@ -11,4 +11,11 @@ import { FoodExpiresSoon } from '@3-steps/interfaces';
 })
 export class DiscountItemComponent {
     @Input({ required: true }) item!: FoodExpiresSoon;
+
+    get discountedPrice() {
+        return (
+            this.item.price *
+            (1 - this.item.discountInPercent / 100)
+        ).toFixed(2);
+    }
 }
